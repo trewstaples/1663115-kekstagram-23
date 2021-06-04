9e30b329df53bf8e4aac32b445eecd2a11e8bb0a
LAlex <trewstaples@gmail.com>
2021-06-04 09:47:20 +0300

    Изменяет название функции в main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-function getSun (a, b) {
-  const description = "Сумма двух чисел равна";
+
+function getSum (a, b) {
+  const description = 'Сумма двух чисел равна';
   const sum = a + b;
   return `${description  }${  sum}`;
 }
 
 getSum(1, 2);
+
+
