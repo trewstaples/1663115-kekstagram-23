419a0751d22192f45874b115a548ef68e39a4862
LAlex <trewstaples@gmail.com>
2021-06-15 18:41:30 +0300

    Добавляет изменения в функцию второго практического задания по замечаниям наставника

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 //Функция, возвращающая случайное целое число из переданного диапозона включительно
 //имя_функции(от, до); // Результат: целое число из диапазона "от...до"
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if (max>min) {
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
-  return ('Введите корректное значение');
+function checkStringLength (string, length) {
+  return string.length <= length;
 }
-getRandomIntInclusive(0,100);
-//Функцию сам придумать не смог, поэтому взял из статьи с MDN. Вот ссылка - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+checkStringLength (1,3);
+//Решил оставить функцию из файла check-string от академии, так как там есть объяснение )))
 
 //Функция для проверки максимальной длины строки
 //имя_функции(проверяемая_строка, максимальная_длина);
-function checkLineLength (str, maxLength) {
-  return str.length <= maxLength;
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-checkLineLength('HTML', 8);
+getRandomPositiveInteger(1,100);
 //Эту функцию написал сам
